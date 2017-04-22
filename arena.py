9646fe17bd012a9e56fa17048e195b923f40093a
kenny <kennyg1980@gmail.com>
2017-04-21 23:45:48 -0400

    moved game_menu.py to main.py and deleted game_menu.py

--- arena.py
+++ arena.py
@@ -1,5 +1,5 @@
 import config
-import game_menu
+
 
 def arenafight():
     print("Welcome %s The Arena has not started yet! Please be patient." % (config.hero_class))
--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-import game_menu
+
 import arena
+import character_creation
+import config
+import game_load
+
+
+def gamemenu():
+    main_menu_choice = "0"
+    while main_menu_choice < "3":
+        print("Main Menu")
+        print("1. New Game\n")
+        print("2. Load Game\n")
+        print("3. Exit\n")
+        print("Please make a choice!")
+        main_menu_choice = input(">>")
+        print(main_menu_choice)
+
+
+        if main_menu_choice == "1":
+            character_creation.create_character()
+            "Now to start the game"
+        elif main_menu_choice == "2":
+            print("Feature in progress")
+            game_load.load_game_settings()
+            print("I am a %r %r !!!\nMy level is %s and i am ready to enter the arena!" % (config.hero_gender, config.hero_class, config.hero_base_level))
+
+            print("Now to start the game")
+        else:
+            print("bye")
+            exit(0)
+
+    return
+
+
+
 
 
 print("Welcome to Hero Fight Arena\n")
 
 
-game_menu.gamemenu()
+
+
+
+
+gamemenu()
 
 
 
